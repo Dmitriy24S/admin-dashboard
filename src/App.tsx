d704feaea7a790be69223cfbf64f19ddae544cd4
Dmitriy24S <devilins24@gmail.com>
2023-03-20 13:08:10 +0200

    feat: setup MUI theme, misc.
    * misc: remove files, add mock data, etc.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { ColorModeContext, useMode } from './theme/theme'
 
 function App() {
+  const [theme, colorMode] = useMode()
+  console.log('theme', theme)
+  // theme: {
+  // breakpoints: {keys: Array(5), values: {…}, up: ƒ, down: ƒ, between: ƒ, …}
+  // components: {}
+  // direction: "ltr"
+  // mixins: {toolbar: {…}}
+  // palette: {mode: 'dark', primary: {…}, secondary: {…}, neutral: {…}, background: {…}, …}
+  console.log('colorMode', colorMode)
+  // {toggleColorMode: ƒ}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className='app'>
+          <header className='App-header'>
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className='App-link'
+              href='https://reactjs.org'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Learn React
+            </a>
+          </header>
+          <main className='content'>Content</main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
 }
 
-export default App;
+export default App
