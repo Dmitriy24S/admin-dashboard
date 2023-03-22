e5904c69add11e44e410b58112a6f3bef409c983
Dmitriy24S <devilins24@gmail.com>
2023-03-22 11:52:55 +0200

    feat: setup routes (blank components)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import { CssBaseline, ThemeProvider } from '@mui/material'
+import { Route, Routes } from 'react-router-dom'
+import Bar from './scenes/bar'
+import Calendar from './scenes/calendar'
+import Contacts from './scenes/contacts'
+import Dashboard from './scenes/dashboard'
+import FAQ from './scenes/faq'
+import Form from './scenes/form'
+import Geography from './scenes/geography'
+import Sidebar from './scenes/global/Sidebar'
 import Topbar from './scenes/global/Topbar'
+import Invoices from './scenes/invoices'
+import Line from './scenes/line'
+import Pie from './scenes/pie'
+import Team from './scenes/team'
 import { ColorModeContext, useMode } from './theme/theme'
 
 function App() {
@@ -19,8 +32,23 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className='app'>
-          <Topbar />
-          <main className='content'>Content</main>
+          <main className='content'>
+            <Topbar />
+            <Sidebar />
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/team' element={<Team />} />
+              <Route path='/contacts' element={<Contacts />} />
+              <Route path='/invoices' element={<Invoices />} />
+              <Route path='/form' element={<Form />} />
+              <Route path='/bar' element={<Bar />} />
+              <Route path='/pie' element={<Pie />} />
+              <Route path='/line' element={<Line />} />
+              <Route path='/faq' element={<FAQ />} />
+              <Route path='/geography' element={<Geography />} />
+              <Route path='/calendar' element={<Calendar />} />
+            </Routes>
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
